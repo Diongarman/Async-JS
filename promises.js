6bb2ff964217f3a29ce95ae6d5ed13273ec91c45
Dion Garman <diongarman@gmail.com>
2020-01-26 16:36:00 +0000

    Promises race with setTimeout to furtehr demonstrate

--- promises.js
+++ promises.js
@@ -1,33 +1,49 @@
 const willCleanRoom = new Promise((resolve, reject) => {
 
 
+    setTimeout(
+        () => {
             let cleanRoom = true;
             if (cleanRoom) {
                 resolve('room is clean');
             } else {
                 reject('room is not clean');
             }
-        } 
+        } ,3000);
 
-);
-
+    }
+)
 
 const removeGarbage = new Promise((resolve, reject) => {
-    let garbageRemoved = true;
-    if (garbageRemoved) {
-        resolve('Rubbish removed');
-    } else {
-        reject('Rubbish not removed');
-    }
+
+    setTimeout(
+        
+        () => {
+            let garbageRemoved = true;
+            if (garbageRemoved) {
+                resolve('Rubbish removed');
+            } else {
+                reject('Rubbish not removed');
+            }
+
+        }, 3001)
+
+
 });
 
 const getReward = new Promise((resolve, reject) => {
-    let ellegible = true;
-    if (ellegible) {
-        resolve('Got a reward!');
-    } else {
-        reject(':( no reward for you');
-    }
+
+    setTimeout(() => {
+        let ellegible = true;
+        if (ellegible) {
+            resolve('Got a reward!');
+        } else {
+            reject(':( no reward for you');
+        }
+
+    }, 2999)
+
+
 });
 
 Promise.race([willCleanRoom, removeGarbage, getReward]).then((messages) => {
